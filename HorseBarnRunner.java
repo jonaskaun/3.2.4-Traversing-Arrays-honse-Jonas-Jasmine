51dfdc33a015f1bae7b9e92f53cea1120514d941
Jasmine <jasmine.cole@crpusd.org>
2026-02-13 09:03:35 -0800

    6 & 7

--- HorseBarnRunner.java
+++ HorseBarnRunner.java
@@ -16,5 +16,15 @@ public class HorseBarnRunner
     {
       System.out.println(h);
     }
+
+    int numSpaces = barnSpaces.size();
+    for (int i = 0; i < numSpaces; i++)
+      {
+        Horse h = barnSpaces.get(i);
+        if (h.getName().equals("Patches"))
+        {
+          System.out.println("Bye bye " +  barnSpaces.remove(i));
+        }
+      }
   }
 } 
